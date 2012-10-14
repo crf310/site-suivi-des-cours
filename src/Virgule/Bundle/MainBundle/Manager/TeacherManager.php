1abd6b467ddb769e4b5160c1cbe0d9822b18e6da
Guillaume Lucazeau <glucazeau@gmail.com>
2012-10-15 00:33:51 +0200

    Create Manager and Repository for Teacher entity

---
+++ src/Virgule/Bundle/MainBundle/Manager/TeacherManager.php
@@ -0,0 +1,12 @@
+<?php
+ 
+namespace Virgule\MainBundle\Manager;
+ 
+use Doctrine\ORM\EntityManager;
+use Virgule\Bundle\MainBundle\Manager\BaseManager;
+use Virgule\Bundle\MainBundle\Entity\Teacher;
+ 
+class TeacherManager extends BaseManager
+{
+    
+}
