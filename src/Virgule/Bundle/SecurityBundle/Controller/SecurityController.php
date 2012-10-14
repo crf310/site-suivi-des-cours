e2274b065c7b74f3ec04c8fed79bc317dd9db66f
Guillaume Lucazeau <glucazeau@gmail.com>
2012-10-15 01:36:34 +0200

    Login form

---
+++ src/Virgule/Bundle/SecurityBundle/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Virgule\Bundle\SecurityBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends Controller {
+
+    public function loginAction() {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('VirguleSecurityBundle:Security:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+}
