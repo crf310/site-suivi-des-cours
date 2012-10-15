46aa1e8c81c9d6d7f6d48b074c7c531809cad616
Guillaume Lucazeau <glucazeau@gmail.com>
2012-10-15 23:39:55 +0200

    Implements UserInterface

--- src/Virgule/Bundle/MainBundle/Entity/Teacher.php
+++ src/Virgule/Bundle/MainBundle/Entity/Teacher.php
@@ -3,6 +3,7 @@
 namespace Virgule\Bundle\MainBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Virgule\Bundle\MainBundle\Entity\Teacher
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Virgule\Bundle\MainBundle\Repository\TeacherRepository")
  */
-class Teacher
+class Teacher implements UserInterface
 {
     /**
      * @var integer $id
@@ -30,5 +31,25 @@ class Teacher
     public function getId()
     {
         return $this->id;
+    }
+
+    public function eraseCredentials() {
+        
+    }
+
+    public function getPassword() {
+        
+    }
+
+    public function getRoles() {
+        
+    }
+
+    public function getSalt() {
+        
+    }
+
+    public function getUsername() {
+        
     }
 }
