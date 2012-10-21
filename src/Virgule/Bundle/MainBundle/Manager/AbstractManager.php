f236194dfb3a79936a284ee4408f9fedf8aedb59
Guillaume Lucazeau <glucazeau@gmail.com>
2012-10-21 13:56:32 +0200

    Add Abstract Manager

---
+++ src/Virgule/Bundle/MainBundle/Manager/AbstractManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Virgule\MainBundle\Manager;
+
+abstract class BaseManager
+{
+    protected function persistAndFlush($entity)
+    {
+        $this->em->persist($entity);
+        $this->em->flush();
+    }
+}
+
+?>
