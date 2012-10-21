78bfd02bb8dfeda2fcb89abaf98664245ce7ec69
Guillaume Lucazeau <glucazeau@gmail.com>
2012-10-21 17:18:47 +0200

    Country  tester and views

---
+++ src/Virgule/Bundle/MainBundle/Tests/Controller/CountryControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Virgule\Bundle\MainBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CountryControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Go to the list view
+        $crawler = $client->request('GET', '/country/');
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        // Go to the show view
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+    }
+    */
+}
