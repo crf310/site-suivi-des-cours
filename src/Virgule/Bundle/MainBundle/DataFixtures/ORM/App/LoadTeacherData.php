d7773442a3bbc18e756f9e085f2c490df5ba412d
Guillaume Lucazeau <glucazeau@gmail.com>
2012-12-13 00:47:40 +0100

    Separate demo/app fixtures

--- src/Virgule/Bundle/MainBundle/DataFixtures/ORM/App/LoadTeacherData.php
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/App/LoadTeacherData.php
@@ -19,22 +19,7 @@ class LoadTeacherData implements FixtureInterface
         $userAdmin->setUsername('root');
         $userAdmin->setPassword('root1234');
         
-        $prof1 = new Teacher();
-        $prof1->setUsername("prof1");
-        $prof1->setPassword("password");
-        $prof1->setFirstName("Henry");
-        $prof1->setLastName("Jones");
-        $prof1->setEmailAddress("henry.jones@example.com");
-        
-        $prof2 = new Teacher();
-        $prof2->setUsername("prof2");
-        $prof2->setPassword("password");
-        $prof2->setFirstName("John");
-        $prof2->setLastName("Keating");
-        $prof2->setEmailAddress("john.keating@example.com");
-        
         $manager->persist($userAdmin);
-        $manager->persist($prof1);
                 
         $manager->flush();
     }
---
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadTeacherData.php
@@ -0,0 +1,45 @@
+<?php
+namespace Virgule\Bundle\MainBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Virgule\Bundle\MainBundle\Entity\Teacher;
+
+/**
+ * Description of LoadTeacherData
+ *
+ * @author Guillaume Lucazeau
+ */
+
+class LoadTeacherData implements FixtureInterface {
+    public function load(ObjectManager $manager) {       
+        $prof1 = new Teacher();
+        $prof1->setUsername("prof1");
+        $prof1->setPassword("password");
+        $prof1->setFirstName("Henry");
+        $prof1->setLastName("Jones");
+        $prof1->setEmailAddress("henry.jones@example.com");
+        
+        $prof2 = new Teacher();
+        $prof2->setUsername("prof2");
+        $prof2->setPassword("password");
+        $prof2->setFirstName("John");
+        $prof2->setLastName("Keating");
+        $prof2->setEmailAddress("john.keating@example.com");
+
+        $prof3 = new Teacher();
+        $prof3->setUsername("prof3");
+        $prof3->setPassword("password");
+        $prof3->setFirstName("Walter");
+        $prof3->setLastName("Lewin");
+        $prof3->setEmailAddress("walter.lewin@example.com");
+        
+        $manager->persist($prof1);
+        $manager->persist($prof2);
+        $manager->persist($prof3);
+                
+        $manager->flush();
+    }
+}
+
+?>
