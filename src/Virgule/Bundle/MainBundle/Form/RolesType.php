3e4c974e5bd8ed1565872ae924b20ccb187ed96b
Guillaume Lucazeau <glucazeau@jahia.com>
2012-12-13 17:26:41 +0100

    Add roles form/controller/test classes

---
+++ src/Virgule/Bundle/MainBundle/Form/RolesType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Virgule\Bundle\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RolesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label')
+            ->add('code')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Virgule\Bundle\MainBundle\Entity\Roles'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'virgule_bundle_mainbundle_rolestype';
+    }
+}
