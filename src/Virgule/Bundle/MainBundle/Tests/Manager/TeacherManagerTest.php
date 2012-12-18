128e724b430e9f6e0ea98d8368c673bb79934cb9
Guillaume Lucazeau <glucazeau@jahia.com>
2012-12-18 17:47:50 +0100

    Init teacher manager test class

---
+++ src/Virgule/Bundle/MainBundle/Tests/Manager/TeacherManagerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Virgule\Bundle\MainBundle\Tests\Manager;
+
+namespace Virgule\Bundle\MainBundle\Manager\TeacherManager;
+
+/**
+ * Description of TeacherManagerTest
+ *
+ * @author guillaume
+ */
+class TeacherManagerTest extends \PHPUnit_Framework_TestCase {
+    public function testTrue() {
+        $this->assertTrue(true);
+    }
+}
+
+?>
