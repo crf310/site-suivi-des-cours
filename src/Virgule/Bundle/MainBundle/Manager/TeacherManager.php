af72d0409a8bcabc74740f4d6675e9a9d4e7910f
Guillaume Lucazeau <glucazeau@gmail.com>
2012-12-18 01:19:31 +0100

    Remove unused import

--- src/Virgule/Bundle/MainBundle/Manager/TeacherManager.php
+++ src/Virgule/Bundle/MainBundle/Manager/TeacherManager.php
@@ -4,7 +4,6 @@ namespace Virgule\Bundle\MainBundle\Manager;
  
 use Doctrine\ORM\EntityManager;
 use Virgule\Bundle\MainBundle\Manager\BaseManager;
-use Virgule\Bundle\MainBundle\Repository\TeacherRepository;
 use \Virgule\Bundle\MainBundle\Entity\Teacher;
  
 class TeacherManager extends BaseManager {
