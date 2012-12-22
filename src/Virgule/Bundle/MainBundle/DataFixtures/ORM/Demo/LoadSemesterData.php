f66f05aa6de3e41204d93bb3b1dd22fc2cbe5c6e
Guillaume Lucazeau <glucazeau@gmail.com>
2012-12-22 15:03:42 +0100

    Add organization branch for demo semesters

--- src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadSemesterData.php
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadSemesterData.php
@@ -17,14 +17,17 @@ class LoadSemesterData extends AbstractFixture implements OrderedFixtureInterfac
         $semester1 = new Semester();
         $semester1->setStartDate(new \DateTime('2010-09-25'));
         $semester1->setEndDate(new \DateTime('2011-06-15'));
+        $semester1->setOrganizationBranch($this->getReference('deleg-3-10'));
 
         $semester2 = new Semester();
         $semester2->setStartDate(new \DateTime('2011-09-02'));
         $semester2->setEndDate(new \DateTime('2012-06-02'));
+        $semester1->setOrganizationBranch($this->getReference('deleg-3-10'));
         
         $semester3 = new Semester();
         $semester3->setStartDate(new \DateTime('2012-09-02'));
         $semester3->setEndDate(new \DateTime('2013-06-01'));
+        $semester1->setOrganizationBranch($this->getReference('deleg-3-10'));
         
         $manager->persist($semester1);
         $manager->persist($semester2);
