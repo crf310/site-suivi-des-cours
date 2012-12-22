e71bbb2574794574915818d795d7d61c94d92291
Guillaume Lucazeau <glucazeau@gmail.com>
2012-12-22 14:54:27 +0100

    Store organization branch in session

--- src/Virgule/Bundle/MainBundle/Event/LoginEventListener.php
+++ src/Virgule/Bundle/MainBundle/Event/LoginEventListener.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Virgule\Bundle\MainBundle\Event;
 
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
@@ -6,27 +7,37 @@ use Virgule\Bundle\MainBundle\Entity\Teacher as Teacher;
 use Doctrine\ORM\EntityManager;
 
 class LoginEventListener {
-    
+
     protected $entityManager;
-     
+
     public function __construct(EntityManager $em) {
         $this->entityManager = $em;
     }
-    
+
     /**
      * Catches the login of a user and does something with it
      *
      * @param \Symfony\Component\Security\Http\Event\InteractiveLoginEvent $event
      * @return void
      */
-    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
-    {
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event) {
         $token = $event->getAuthenticationToken();
         if ($token && $token->getUser() instanceof Teacher) {
-           $teacher = $token->getUser();
-           $teacher->setLastConnectionDate(new \DateTime('now'));
-           $this->entityManager->flush();
+            $teacher = $token->getUser();
+            $teacher->setLastConnectionDate(new \DateTime('now'));
+            $this->entityManager->flush();
+
+            $request = $event->getRequest();
+            $session = $request->getSession();
+
+            $organizationBranchId = $request->get('organization_branch_id');
+            $organizationBranch = $this->entityManager->getRepository('Virgule\Bundle\MainBundle\Entity\OrganizationBranch')->loadOne($organizationBranchId);
+
+            $session->set('organizationBranchId', $organizationBranchId);
+            $session->set('organizationBranchName', $organizationBranch->getName());
         }
     }
+
 }
+
 ?>
