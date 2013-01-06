194c9625f6a935b1d2580290bf8fa641fe079c7b
Guillaume Lucazeau <glucazeau@gmail.com>
2013-01-06 21:38:17 +0100

    Init manager

---
+++ src/Virgule/Bundle/MainBundle/Manager/OrganizationBranchManager.php
@@ -0,0 +1,21 @@
+<?php
+ 
+namespace Virgule\Bundle\MainBundle\Manager;
+ 
+use Doctrine\ORM\EntityManager;
+use Virgule\Bundle\MainBundle\Manager\BaseManager;
+use \Virgule\Bundle\MainBundle\Entity\OrganizationBranch;
+ 
+class OrganizationBranchManager extends BaseManager {
+    protected $em;
+
+    public function __construct(EntityManager $em) {
+        $this->em = $em;
+    }
+    
+    public function getRepository() {
+        return $this->em->getRepository('VirguleMainBundle:OrganizationBranch');
+    }
+}
+
+?>
