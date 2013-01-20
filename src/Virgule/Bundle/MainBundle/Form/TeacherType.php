e30d2c15c1d1c5c1d2e95844d9ca72e890368cb7
Guillaume Lucazeau <glucazeau@gmail.com>
2013-01-21 00:28:33 +0100

    Format class teacher form

--- src/Virgule/Bundle/MainBundle/Form/TeacherType.php
+++ src/Virgule/Bundle/MainBundle/Form/TeacherType.php
@@ -6,42 +6,40 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class TeacherType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class TeacherType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('isActive')
-            ->add('lastName')
-            ->add('firstName')
-            ->add('phoneNumber')
-            ->add('cellphoneNumber')
-            ->add('emailAddress')
-            ->add('username')
-            ->add('password')
-            ->add('registrationDate','date',array(
-	            'widget' => 'single_text',
-	            'format' => 'dd-MM-yyyy',
-	            'attr' => array('class' => 'date')
-	        ))
-            ->add('role', 'entity', array(
-                'class' => 'VirguleMainBundle:Roles',
-                'expanded' => true,
-                'multiple'  => false,
-                'property' => 'label',
-                'property_path' => false,
+                ->add('isActive')
+                ->add('lastName')
+                ->add('firstName')
+                ->add('phoneNumber')
+                ->add('cellphoneNumber')
+                ->add('emailAddress')
+                ->add('username')
+                ->add('password')
+                ->add('registrationDate', 'date', array(
+                    'widget' => 'single_text',
+                    'format' => 'dd-MM-yyyy',
+                    'attr' => array('class' => 'date')
+                ))
+                ->add('role', 'entity', array(
+                    'class' => 'VirguleMainBundle:Roles',
+                    'expanded' => true,
+                    'multiple' => false,
+                    'property' => 'label',
+                    'property_path' => false,
                 ));
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Virgule\Bundle\MainBundle\Entity\Teacher'
         ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'virgule_bundle_mainbundle_teachertype';
     }
+
 }
