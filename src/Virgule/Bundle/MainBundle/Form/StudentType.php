be7691a077909cdd9bc8dd4020640cae5d3f4c60
Guillaume Lucazeau <glucazeau@gmail.com>
2013-01-21 00:28:46 +0100

    Disable some fields

--- src/Virgule/Bundle/MainBundle/Form/StudentType.php
+++ src/Virgule/Bundle/MainBundle/Form/StudentType.php
@@ -6,53 +6,45 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class StudentType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class StudentType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('registrationDate')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('birthdate')
-            ->add('phoneNumber')
-            ->add('cellphoneNumber')
-            ->add('address')
-            ->add('zipcode')
-            ->add('city')
-            ->add('gender')
-            ->add('nationality')
-            ->add('maritalStatus')
-            ->add('commentaires')
-            ->add('registringDate')
-            ->add('picturePath')
-            ->add('arrivalDate')
-            ->add('scholarized')
-            ->add('profession')
-            ->add('scholarizedInTheCountry')
-            ->add('scholarizedInAForeignCountry')
-            ->add('scholarizationLevel')
-            ->add('emergencyContactLastname')
-            ->add('emergencyContactFirstname')
-            ->add('emergencyContactPhoneNumber')
-            ->add('emergencyContactConnectionType')
-            ->add('fkProposedLevel')
-            ->add('fkNativeCountry')
-            ->add('fkScholarizationLanguage')
-            ->add('fkMotherTongue')
-            ->add('fkWelcomedByTeacher')
+                //->add('registrationDate')
+                ->add('lastname')
+                ->add('firstname')
+        /*
+          ->add('birthdate')
+          ->add('phoneNumber')
+          ->add('cellphoneNumber')
+          ->add('address')
+          ->add('zipcode')
+          ->add('city')
+          ->add('gender')
+          ->add('nationality')
+          ->add('maritalStatus')
+          ->add('picturePath')
+          ->add('arrivalDate')
+          ->add('scholarized')
+          ->add('profession')
+          ->add('scholarizedInTheCountry')
+          ->add('scholarizedInAForeignCountry')
+          ->add('scholarizationLevel')
+          ->add('emergencyContactLastname')
+          ->add('emergencyContactFirstname')
+          ->add('emergencyContactPhoneNumber')
+          ->add('emergencyContactConnectionType') */
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Virgule\Bundle\MainBundle\Entity\Student'
         ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'virgule_bundle_mainbundle_studenttype';
     }
+
 }
