2ca7f58a4a0437a19f300e343f40e87b76c20325
Guillaume Lucazeau <glucazeau@jahia.com>
2013-01-22 19:05:23 +0100

    Create abstract controller

---
+++ src/Virgule/Bundle/MainBundle/Controller/AbstractVirguleController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Virgule\Bundle\MainBundle\Controller;
+
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Exception\NotValidCurrentPageException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Description of VirguleController
+ *
+ * @author guillaume
+ */
+abstract class AbstractVirguleController extends Controller {
+     
+    public function paginate($entities, $page=1) {
+        $pagerfanta = new Pagerfanta(new ArrayAdapter($entities));
+        $pagerfanta->setMaxPerPage($this->container->parameters['pager_nb_results']);
+
+        try {
+            $pagerfanta->setCurrentPage($page);
+        } catch (NotValidCurrentPageException $e) {
+            throw new NotFoundHttpException();
+        }
+
+        return array('entities' => $pagerfanta);        
+    }
+}
+
+?>
