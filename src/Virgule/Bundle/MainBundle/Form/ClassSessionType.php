39a9e13e0a5c7c8e686fdef805f1d60c61807333
Guillaume Lucazeau <glucazeau@gmail.com>
2013-01-24 23:00:50 +0100

    Init Class Session create form wth CKEditor

--- src/Virgule/Bundle/MainBundle/Form/ClassSessionType.php
+++ src/Virgule/Bundle/MainBundle/Form/ClassSessionType.php
@@ -6,28 +6,41 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ClassSessionType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class ClassSessionType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $now = new \DateTime('now');
+        $sNow = $now->format('d/m/Y');
         $builder
-            ->add('date')
-            ->add('summary')
-            ->add('fkClassId')
-            ->add('fkSessionTeacherId')
-            ->add('fkSummaryTeacherId')
-        ;
+                ->add('date', 'date', array(
+                    'widget' => 'single_text',
+                    'format' => 'dd-MM-yyyy',
+                    'attr' => array('class' => 'date', 'value' => $sNow)))
+                ->add('summary', 'ckeditor', array(
+                        'config' => array(
+                            'toolbar' => array(
+                                array(
+                                    'name'  => 'document',
+                                    'items' => array('Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates'),
+                                ),
+                                '/',
+                                array(
+                                    'name'  => 'basicstyles',
+                                    'items' => array('Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat'),
+                                ),
+                            ),
+                            'ui_color' => '#ffffff'
+                        )));
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Virgule\Bundle\MainBundle\Entity\ClassSession'
         ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'virgule_bundle_mainbundle_classsessiontype';
     }
+
 }
