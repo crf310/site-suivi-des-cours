d4476b13d14e709391cee199ff5f80e50fa5fbbc
Guillaume Lucazeau <glucazeau@gmail.com>
2013-01-30 00:05:29 +0100

    Init add course form

--- src/Virgule/Bundle/MainBundle/Form/CourseType.php
+++ src/Virgule/Bundle/MainBundle/Form/CourseType.php
@@ -6,32 +6,42 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class CourseType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class CourseType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('dayOfWeek')
-            ->add('startTime')
-            ->add('endTime')
-            ->add('alternateStartdate')
-            ->add('alternateEnddate')
-            ->add('fkLevelId')
-            ->add('fkSemesterId')
-            ->add('fkTeacherId')
-            ->add('fkOrganizationBranch')
+                ->add('dayOfWeek', 'choice', array(
+                    'expanded' => false,
+                    'choices' => array('1' => 'Lundi', 'Mardi', 'Mercredi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'),
+                    'data' => '1',
+                ))
+                ->add('startTime')
+                ->add('endTime')
+                ->add('alternateStartdate', 'date', array(
+                    'widget' => 'single_text',
+                    'format' => 'dd/MM/yyyy',
+                    'attr' => array('class' => 'date'),
+                    'required'  => false
+                ))
+                ->add('alternateEnddate', 'date', array(
+                    'widget' => 'single_text',
+                    'format' => 'dd/MM/yyyy',
+                    'attr' => array('class' => 'date'),
+                    'required'  => false
+                ))
+                ->add('classLevel')
+                ->add('teachers')
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Virgule\Bundle\MainBundle\Entity\Course'
         ));
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'virgule_bundle_mainbundle_coursetype';
     }
+
 }
