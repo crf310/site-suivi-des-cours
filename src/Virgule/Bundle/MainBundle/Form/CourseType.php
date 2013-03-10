7398e0bf8975bf481c659a97b3d549016f1ef14a
Guillaume Lucazeau <glucazeau@gmail.com>
2013-03-10 15:40:15 +0100

    Add fullname getter

--- src/Virgule/Bundle/MainBundle/Form/CourseType.php
+++ src/Virgule/Bundle/MainBundle/Form/CourseType.php
@@ -1,36 +1,62 @@
 <?php
 
 namespace Virgule\Bundle\MainBundle\Form;
+use Doctrine\ORM\EntityRepository;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Virgule\Bundle\MainBundle\Repository\TeacherRepository;
 
 class CourseType extends AbstractType {
 
+    private $organizationBranchId;
+    private $teacherRepository;
+
+    public function __construct(TeacherRepository $teacherRepository, $organizationBranchId) {
+        $this->teacherRepository = $teacherRepository;
+        $this->organizationBranchId = $organizationBranchId;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
                 ->add('dayOfWeek', 'choice', array(
                     'expanded' => false,
                     'choices' => array('1' => 'Lundi', 'Mardi', 'Mercredi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'),
                     'data' => '1',
+                    'attr' => array('class' => 'tiny-select'),
                 ))
                 ->add('startTime')
                 ->add('endTime')
                 ->add('alternateStartdate', 'date', array(
                     'widget' => 'single_text',
                     'format' => 'dd/MM/yyyy',
-                    'attr' => array('class' => 'date'),
+                    'attr' => array('class' => 'datepicker','data-date-format' => 'dd/mm/yyyy'),
                     'required'  => false
                 ))
                 ->add('alternateEnddate', 'date', array(
                     'widget' => 'single_text',
                     'format' => 'dd/MM/yyyy',
-                    'attr' => array('class' => 'date'),
+                    'attr' => array('class' => 'datepicker','data-date-format' => 'dd/mm/yyyy'),
                     'required'  => false
+                ))             
+                ->add('classLevel', 'entity', array(
+                    'class' => 'VirguleMainBundle:ClassLevel',
+                    'expanded' => false,
+                    'multiple' => false,
+                    'property' => 'label',
+                    'property_path' => 'classlevel',            
+                    'attr' => array('class' => 'tiny-select')
+                 ))
+                ->add('teachers', 'entity', array(
+                    'class' => 'VirguleMainBundle:Teacher',
+                    'query_builder' =>  $this->teacherRepository->getAvailableTeachersQueryBuilder($this->organizationBranchId, true),
+                    'expanded' => false,
+                    'multiple' => true,
+                    'property' => 'fullname',
+                    'property_path' => 'teachers',            
+                    'attr' => array('class' => 'big-select')
                 ))
-                ->add('classLevel')
-                ->add('teachers')
         ;
     }
 
@@ -43,5 +69,5 @@ class CourseType extends AbstractType {
     public function getName() {
         return 'virgule_bundle_mainbundle_coursetype';
     }
-
+    
 }
