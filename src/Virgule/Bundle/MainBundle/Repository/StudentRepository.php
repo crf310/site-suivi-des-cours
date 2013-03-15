a264385b606b239f061261ba89191390f7a74fa9
Guillaume Lucazeau <glucazeau@jahia.com>
2013-03-15 19:47:04 +0100

    Update repository (broken)

--- src/Virgule/Bundle/MainBundle/Repository/StudentRepository.php
+++ src/Virgule/Bundle/MainBundle/Repository/StudentRepository.php
@@ -15,54 +15,59 @@ use Doctrine\ORM\Query;
 class StudentRepository extends EntityRepository {
 
     private function createDefaultQueryBuilder() {
-        return $this->createQueryBuilder('s');
+        return $this->createQueryBuilder('s')->add('orderBy', 's.lastname ASC, s.firstname ASC');
     }
     
-    public function getBasicQueryBuilder() {
+    private function getBasicQueryBuilder() {
         $qb = $this
             ->createDefaultQueryBuilder()
-            ->addSelect('s.id, s.firstname as firstname, s.lastname as lastname, s.gender as gender, s.phoneNumber as phoneNumber, s.cellphoneNumber')
+            ->addSelect('s.id, s.firstname as firstname, s.lastname as lastname, s.gender as gender, s.phoneNumber as phoneNumber, s.cellphoneNumber, s.registrationDate')
             ->addSelect('c.isoCode, c.label')
-            ->innerJoin('s.nativeCountry', 'c')
-            ->add('orderBy', 's.lastname ASC, s.firstname ASC');
+            ->innerJoin('s.nativeCountry', 'c');
         return $qb;
     }
     
+    public function getQueryBuilderForStudentEnrolledInCourses(Array $courseIds) {
+        $ids = implode(',', $courseIds);
+        $qb = $this
+                ->createDefaultQueryBuilder()
+                ->innerJoin('s.courses', 'c2', 'WITH', 'c2.id IN (:coursesIds)')
+                ->setParameter('coursesIds', $ids);
+        return $qb;
+    }
+        
+    private function addCommentCount($queryBuilder) {
+        $queryBuilder
+            ->addSelect('count(cm.id) as nb_comments')
+            ->leftJoin('s.comments', 'cm')
+            ->add('groupBy', 's.id');
+        return $queryBuilder;
+    }
     /**
      * Select all students enrolled in a class of the selected semester
      * @return type
      */
     public function loadAll($semesterId) {
-        $q = $this->getBasicQueryBuilder()
-                ->addSelect('t.id as teacher_id, t.firstName as teacher_firstName, t.lastName as teacher_lastName, count(cm.id) as nb_comments, s.registrationDate')
+        $qb = $this->getBasicQueryBuilder()
+                ->addSelect('t.id as teacher_id, t.firstName as teacher_firstName, t.lastName as teacher_lastName')
                 ->addSelect('c2.id as course_id, l.label as level')
                 ->innerJoin('s.courses', 'c2')
                 ->innerJoin('c2.classLevel', 'l')
                 ->leftJoin('s.welcomedByTeacher', 't')
-                ->leftJoin('s.comments', 'cm')
                 ->where('c2.semester = :semesterId')
                 ->add('orderBy', 's.lastname ASC, s.firstname ASC')
-                ->add('groupBy', 's.id')
-                ->setParameter('semesterId', $semesterId)
-                ->getQuery()
-        ;
+                ->setParameter('semesterId', $semesterId);
+        $this->addCommentCount($qb);
+        
+        $q = $qb->getQuery();
         $students = $q->execute(array(), Query::HYDRATE_ARRAY);
         return $students;
     }
-    
-    public function getQueryBuilderForStudentEnrolledInCourses(Array $courseIds) {
-        $ids = implode(',', $courseIds);
-        $q = $this
-                ->createDefaultQueryBuilder()
-                ->innerJoin('s.courses', 'c2', 'WITH', 'c2.id IN (:coursesIds)')
-                ->setParameter('coursesIds', $ids)
-        ;
-        return $q;
-    }
 
     public function loadAllEnrolledInCourses(Array $courseIds) {
-        $q = $this->getQueryBuilderForStudentEnrolledInCourses($courseIds);
-        return $q->getQuery()->execute(array(), Query::HYDRATE_ARRAY);
+        $qb = $this->getQueryBuilderForStudentEnrolledInCourses($courseIds);
+        $qb = $this->addCommentCount($qb);
+        return $qb->getQuery()->execute(array(), Query::HYDRATE_ARRAY);
     }
     
     /**
@@ -75,10 +80,11 @@ class StudentRepository extends EntityRepository {
     }
 
     public function loadAllPresentAtClassSession($classSessionId) {
-        $q = $this->getBasicQueryBuilder()
+        $qb = $this->getBasicQueryBuilder()
                 ->innerJoin('s.classSessions', 'cs')
                 ->where('cs.id = :classSessionId')
                 ->setParameter('classSessionId', $classSessionId);
+        $this->addCommentCount($qb);
         
         return $q->getQuery()->execute(array(), Query::HYDRATE_ARRAY);
     }
