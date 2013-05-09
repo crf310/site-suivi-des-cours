d0745d580842be6644e9bc04deafbca5edd8efa4
Guillaume Lucazeau <glucazeau@gmail.com>
2013-05-10 00:56:00 +0200

    Put back customization for JS tables

--- web/js/unicorn.tables.js
+++ web/js/unicorn.tables.js
@@ -3,12 +3,35 @@
  * Diablo9983 -> diablo9983@gmail.com
 **/
 $(document).ready(function(){
-	
-	$('.data-table').dataTable({
-		"bJQueryUI": true,
-		"sPaginationType": "full_numbers",
-		"sDom": '<""l>t<"F"fp>'
-	});
+        $('.data-table').dataTable({
+        "bJQueryUI": true,
+        "sPaginationType": "full_numbers",
+        "sDom": '<""l>t<"F"fp>',
+                        "iDisplayLength": 15,
+                        "aaSorting": [],
+                        "oLanguage": {
+                            "sProcessing": "Traitement en cours...",
+                            "sSearch": "Filtrer&nbsp;:",
+                            "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
+                            "sInfo": "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                            "sInfoEmpty": "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                            "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                            "sInfoPostFix": "",
+                            "sLoadingRecords": "Chargement en cours...",
+                            "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                            "sEmptyTable": "Aucune donnée disponible dans le tableau",
+                            "oPaginate": {
+                                "sFirst": "Premier",
+                                "sPrevious": "Pr&eacute;c&eacute;dent",
+                                "sNext": "Suivant",
+                                "sLast": "Dernier"
+                            },
+                            "oAria": {
+                                "sSortAscending": ": activer pour trier la colonne par ordre croissant",
+                                "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
+                            }
+                        }
+        });
 	
 	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
 	
