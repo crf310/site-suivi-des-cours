a9aba78aca1f6b9c8dd8e42606648caf41953a3a
Guillaume Lucazeau <glucazeau@jahia.com>
2013-05-10 11:09:48 +0200

    Add Sonata Intl bundle Use country filter

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
            
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\IntlBundle\SonataIntlBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
