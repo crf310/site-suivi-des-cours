0e76a7654748956fbfe35979097b107a697c3c84
Guillaume Lucazeau <glucazeau@jahia.com>
2013-05-17 16:23:05 +0200

    Fix issue for repository per level Clean repository

--- src/Virgule/Bundle/MainBundle/Repository/ClassSessionRepository.php
+++ src/Virgule/Bundle/MainBundle/Repository/ClassSessionRepository.php
@@ -13,130 +13,92 @@ use Doctrine\ORM\Query;
  * repository methods below.
  */
 class ClassSessionRepository extends EntityRepository {
-    
+    /* Query builders */
     private function getDefaultQueryBuilder() {
         return $this->createQueryBuilder('c');
     }
-   
-    private function getBasicQueryBuilder() {
-        
-    }
-    public function loadAllClassSessionByTeacher($semesterId, $teacherId, $limit = null) {
-        $qb = $this->getDefaultQueryBuilder()
-            ->addSelect('c.id, c.reportDate, c.sessionDate, count(cm.id) as nb_comments')
-            ->addSelect('cl.label as classLevelLabel, cl.htmlColorCode as classLevelHtmlColorCode')
-            ->innerJoin('c.sessionTeacher', 't')
-            ->innerJoin('c.course', 'c2')
-            ->innerJoin('c2.semester', 's')
-            ->innerJoin('c2.classLevel', 'cl')
-            ->leftJoin('c.comments', 'cm')
-            ->where('t.id = :teacherId')             
-            ->andWhere('s.id = :semesterId')
-            ->setParameter('teacherId', $teacherId)
-            ->setParameter('semesterId', $semesterId)
-            ->add('orderBy', 'c.sessionDate DESC')
-            ->add('groupBy', 'c.id')
-        ;
-        
-        if ($limit != null) {
-            $qb->setMaxResults($limit);
-        }
-        
-        $q = $qb->getQuery();
-                
-        $results = $q->execute(array(), Query::HYDRATE_ARRAY);
-        return $results;   
-    }
     
-    public function loadAllClassSessionByClassLevel($classLevelId, $semesterId) {
-        $qb = $this->getDefaultQueryBuilder()
-            ->addSelect('c.id, c.reportDate, c.sessionDate, count(cm.id) as nb_comments')
-            ->innerJoin('c.course', 'c2')
-            ->innerJoin('c2.classLevel', 'cl', 'WITH', 'cl.id = :classLevelId')
-            ->innerJoin('c.sessionTeacher', 't')
-            ->innerJoin('c2.semester', 's', 'WITH', 'c2.id = :semesterId')
-            ->leftJoin('c.comments', 'cm')
-            ->setParameter('classLevelId', $classLevelId)
-            ->setParameter('semesterId', $semesterId)
-            ->add('orderBy', 'c.reportDate DESC')
-            ->add('groupBy', 'c.id')
-        ;
-        
-        $q = $qb->getQuery();
-                
-        $results = $q->execute(array(), Query::HYDRATE_ARRAY);
-        return $results;   
-    }    
-    
-    public function loadAllClassSessionByCourse($courseId, $limit = null) {
-        $qb = $this->getDefaultQueryBuilder()
-            ->addSelect('c.id, c.reportDate, c.sessionDate, count(cm.id) as nb_comments')
-            ->innerJoin('c.course', 'c2')
-            ->innerJoin('c.sessionTeacher', 't')
-            ->leftJoin('c.comments', 'cm')
-            ->where('c2.id = :courseId')
-            ->setParameter('courseId', $courseId)
-            ->add('orderBy', 'c.reportDate DESC')
-            ->add('groupBy', 'c.id')
-        ;
-        
-        if ($limit != null) {
-            $qb->setMaxResults($limit);
-        }
-        
-        $q = $qb->getQuery();
-                
-        $results = $q->execute(array(), Query::HYDRATE_ARRAY);
-        return $results;   
-    }    
-    
-    public function loadAll($semesterId, $limit = null) {
-        $qb = $this->getDefaultQueryBuilder()
+    private function getBasicQueryBuilder($semesterId = null, $limit = null) {
+         $qb = $this->getDefaultQueryBuilder()
             ->addSelect('c.id as id, c.reportDate, c.sessionDate, c.summary as summary')
             ->addSelect('c2.id as course_id, c2.dayOfWeek as course_dayOfWeek,
                 c2.startTime as course_startTime, c2.endTime as course_endTime')
             ->addSelect('t1.id as sessionTeacher_id, t1.firstName as sessionTeacher_firstName, t1.lastName as sessionTeacher_lastName')
             ->addSelect('t2.id as reportTeacher_id, t2.firstName as reportTeacher_firstName, t2.lastName as reportTeacher_lastName')
             ->addSelect('cl.label as classLevelLabel, cl.htmlColorCode as classLevelHtmlColorCode')
-            ->addSelect('count(st.id) as nb_students')
             ->innerJoin('c.course', 'c2')
             ->innerJoin('c2.classLevel', 'cl')
             ->innerJoin('c2.semester', 's')
             ->innerJoin('c.sessionTeacher', 't1')
             ->innerJoin('c.reportTeacher', 't2')
-            ->leftJoin('c.classSessionStudents', 'st')
-            ->where('s.id = :semesterId')
             ->add('orderBy', 'c.reportDate DESC')
             ->add('groupBy', 'c.id')
             ->setParameter('semesterId', $semesterId);
         
-            if ($limit != null) {
-                $qb->setMaxResults($limit);
-            }
-        
-         $q = $qb->getQuery()
+        if (null !== $semesterId) {
+            $qb->where('s.id = :semesterId');
+        }
+        if ($limit != null) {
+            $qb->setMaxResults($limit);
+        }
+         return $qb;
+    }
+    
+    private function getNbStudentsQueryBuilder($semesterId, $limit = null)  {
+        $qb = $this->getBasicQueryBuilder($semesterId, $limit) 
+                ->leftJoin('c.classSessionStudents', 'st')               
+                ->addSelect('count(st.id) as nb_students');
+        return $qb;
+    }
+    
+    private function getNbCommentsQueryBuilder($semesterId, $limit = null) {
+        $qb = $this->getBasicQueryBuilder($semesterId, $limit)
+                ->leftJoin('c.comments', 'cm')
+                ->addSelect('count(cm.id) as nb_comments');
+        return $qb;
+    }   
+    
+    
+    
+    public function loadAllClassSessionByTeacher($semesterId, $teacherId, $limit = null) {
+        $qb = $this->getNbCommentsQueryBuilder($semesterId, $limit)
+            ->andwhere('t1.id = :teacherId')             
+            ->setParameter('teacherId', $teacherId)
+        ;        
+        $q = $qb->getQuery();                
+        $results = $q->execute(array(), Query::HYDRATE_ARRAY);
+        return $results;   
+    }
+    
+    public function loadAllClassSessionByClassLevel($classLevelId, $semesterId) {
+        $qb = $this->getNbStudentsQueryBuilder($semesterId)
+            ->andWhere('cl.id = :classLevelId')
+            ->setParameter('classLevelId', $classLevelId)
+        ;        
+        $q = $qb->getQuery();                
+        $results = $q->execute(array(), Query::HYDRATE_ARRAY);
+        return $results;   
+    }    
+    
+    public function loadAllClassSessionByCourse($courseId, $limit = null) {
+        $qb = $this->getNbStudentsQueryBuilder(null, $limit)
+            ->andWhere('c2.id = :courseId')   
+            ->setParameter('courseId', $courseId)
         ;
+        $q = $qb->getQuery();                
+        $results = $q->execute(array(), Query::HYDRATE_ARRAY);
+        return $results;   
+    }    
+    
+    public function loadAll($semesterId, $limit = null) {
+        $qb = $this->getNbStudentsQueryBuilder($semesterId, $limit);
+        $q = $qb->getQuery();
         $results = $q->execute(array(), Query::HYDRATE_ARRAY);
         return $results;  
     }
     
     public function loadAllForMiniList($semesterId, $limit = null) {
-        $qb = $this->getDefaultQueryBuilder()
-            ->addSelect('c.id as id, c.reportDate, c.sessionDate')
-            ->addSelect('count(cm.id) as nb_comments')
-            ->addSelect('cl.label as classLevelLabel, cl.htmlColorCode as classLevelHtmlColorCode')
-            ->innerJoin('c.course', 'c2')
-            ->innerJoin('c2.classLevel', 'cl')
-            ->innerJoin('c2.semester', 's')
-            ->leftJoin('c.comments', 'cm')
-            ->where('s.id = :semesterId')
-            ->add('orderBy', 'c.reportDate DESC')
-            ->add('groupBy', 'c.id')
-            ->setParameter('semesterId', $semesterId);
-        
-            if ($limit != null) {
-                $qb->setMaxResults($limit);
-            }
+        $qb = $this->getNbCommentsQueryBuilder($semesterId, $limit);
         
          $q = $qb->getQuery()
         ;
