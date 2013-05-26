c8fbd42f918904bf9bfec4d69ce59595a5fb898a
Guillaume Lucazeau <glucazeau@gmail.com>
2013-05-26 20:34:12 +0200

    Fix entity classname

--- src/Virgule/Bundle/MainBundle/Entity/Language.php
+++ src/Virgule/Bundle/MainBundle/Entity/Language.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="language")
  * @ORM\Entity(repositoryClass="Virgule\Bundle\MainBundle\Repository\LanguageRepository")
  */
-class wLanguage {
+class Language {
 
     /**
      * @var integer $id
