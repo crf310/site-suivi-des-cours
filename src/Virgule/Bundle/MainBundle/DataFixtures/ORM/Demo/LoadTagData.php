6e04b1ecc9056022b9b5414ea6eca53fe86ae8ff
Guillaume Lucazeau <glucazeau@jahia.com>
2013-05-29 17:11:38 +0200

    Add new default tags

--- src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadTagData.php
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadTagData.php
@@ -19,13 +19,13 @@ class LoadTagData extends AbstractFixture implements OrderedFixtureInterface {
         $tag2 = new Tag();
         $tag2->setLabel("Orthographe");
         $tag3 = new Tag();
-        $tag3->setLabel("Singulier");
+        $tag3->setLabel("Singulier/Pluriel");
         $tag4 = new Tag();
-        $tag4->setLabel("Pluriel");
+        $tag4->setLabel("Administratif");
         $tag5 = new Tag();
-        $tag5->setLabel("Masculin");
+        $tag5->setLabel("Masculin/Féminin");
         $tag6 = new Tag();
-        $tag6->setLabel("Féminin");
+        $tag6->setLabel("Accueil");
         $tag7 = new Tag();
         $tag7->setLabel("Calcul");
         $tag8 = new Tag();
@@ -33,14 +33,18 @@ class LoadTagData extends AbstractFixture implements OrderedFixtureInterface {
         $tag9 = new Tag();
         $tag9->setLabel("Evaluation");
         $tag10 = new Tag();
-        $tag10->setLabel("Chansons");
+        $tag10->setLabel("Réunion");
         
         $manager->persist($tag1);
         $manager->persist($tag2);
         $manager->persist($tag3);
         $manager->persist($tag4);
+        $manager->persist($tag5);
         $manager->persist($tag6);
-        $manager->persist($tag1);
+        $manager->persist($tag7);
+        $manager->persist($tag8);
+        $manager->persist($tag9);
+        $manager->persist($tag10);
         
         $manager->flush();
     }
