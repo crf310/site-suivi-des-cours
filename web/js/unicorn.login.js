20eeba2eeeefd7af3773522db77a4956d36d0ede
Guillaume Lucazeau <glucazeau@gmail.com>
2013-11-02 21:55:17 +0100

    Init migration to Bootstrap 3 / Unicorn 2.1.0

--- web/js/unicorn.login.js
+++ web/js/unicorn.login.js
@@ -1,54 +1,68 @@
 /**
  * Unicorn Admin Template
+ * Version 2.1.0
  * Diablo9983 -> diablo9983@gmail.com
 **/
+
+var login = $('#loginform');
+var recover = $('#recoverform');
+var register = $('#registerform');
+var login_recover = $('#loginform, #recoverform');
+var login_register = $('#loginform, #registerform');
+var recover_register = $('#recoverform, #registerform');
+var loginbox = $('#loginbox');
+var userbox = $('#user');
+var animation_speed = 300;
+
 $(document).ready(function(){
 
-    var login = $('#loginform');
-    var recover = $('#recoverform');
-    var register = $('#registerform');
-    var login_recover = $('#loginform, #recoverform');
-    var login_register = $('#loginform, #registerform');
-    var recover_register = $('#recoverform, #registerform');
-    var loginbox = $('#loginbox');
-    var speed = 300;
+    var loc = window.location + '';
+    var ee = loc.split('#');
 
-    $('.flip-link.to-recover').click(function(){
-            login_register.css('z-index','100').fadeTo(speed,0.01,function(){
-            loginbox.animate({'height':'205px'},speed,function(){
-            recover.fadeTo(speed,1).css('z-index','200');
-        });
-    });
-    });
-    $('.flip-link.to-login').click(function(){
-            recover_register.css('z-index','100').fadeTo(speed,0.01,function(){
-            loginbox.animate({'height':'260px'},speed,function(){
-            login.fadeTo(speed,1).css('z-index','200');
-        });
-    });
-    });
-
-    if($.browser.msie == true && $.browser.version.slice(0,3) < 10) {
-        $('input[placeholder]').each(function(){ 
-       
-        var input = $(this);       
-       
-        $(input).val(input.attr('placeholder'));
-               
-        $(input).focus(function(){
-             if (input.val() == input.attr('placeholder')) {
-                 input.val('');
-             }
-        });
-       
-        $(input).blur(function(){
-            if (input.val() == '' || input.val() == input.attr('placeholder')) {
-                input.val(input.attr('placeholder'));
-            }
-        });
-    });
-
-        
-        
+    if(ee[1] == 'recoverform' && ee[1] != undefined){
+        loginbox.css({'height':'183px'});
+        $('#loginform, #registerform').css({'z-index':'100','opacity':'0.01'});
+        $('#recoverform').css({'z-index':'200','opacity':'1','display':'block'});
+    } else if(ee[1] = 'registerform' && ee[1] != undefined) {
+        loginbox.css({'height':'280px'});
+        login_recover.css({'z-index':'100','opacity':'0.01'});
+        register.css({'z-index':'200','opacity':'1','display':'block'});
     }
+
+	$('.flip-link.to-recover').click(function(){
+        switch_container(recover,login_register,183);
+	});
+	$('.flip-link.to-login').click(function(){
+        switch_container(login,recover_register,255);
+	});
+    $('.flip-link.to-register').click(function(){
+        switch_container(register,login_recover,280);
+    });
+
+    $('#loginform').submit(function(e){
+        return true;
+    });
+
+    $('#username, #password').on('keyup',function(){
+        highlight_error($(this));
+    }).focus(function(){
+        highlight_error($(this));
+    }).blur(function(){
+        highlight_error($(this));
+    });
 });
+
+function highlight_error(el) {
+    if(el.val() == '') {
+        el.parent().addClass('has-error');
+    } else {
+        el.parent().removeClass('has-error');
+    }
+}
+function switch_container(to_show,to_hide,cwidth) {
+    to_hide.css('z-index','100').fadeTo(animation_speed,0.01,function(){
+        loginbox.animate({'height':cwidth+'px'},animation_speed,function(){
+            to_show.fadeTo(animation_speed,1).css('z-index','200');
+        });
+    });
+}
--- web/js/unicorn.tables.js
+++ web/js/unicorn.tables.js
@@ -1,52 +1,61 @@
 /**
  * Unicorn Admin Template
+ * Version 2.1.0
  * Diablo9983 -> diablo9983@gmail.com
 **/
+
 $(document).ready(function(){
-        $('.data-table').dataTable({
-        "bJQueryUI": true,
-        "sPaginationType": "full_numbers",
-        "sDom": '<""l>t<"F"fp>',
-                        "iDisplayLength": 15,
-                        "aaSorting": [],
-                        "oLanguage": {
-                            "sProcessing": "Traitement en cours...",
-                            "sSearch": "Filtrer&nbsp;:",
-                            "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
-                            "sInfo": "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-                            "sInfoEmpty": "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-                            "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-                            "sInfoPostFix": "",
-                            "sLoadingRecords": "Chargement en cours...",
-                            "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
-                            "sEmptyTable": "Aucune donnée disponible dans le tableau",
-                            "oPaginate": {
-                                "sFirst": "Premier",
-                                "sPrevious": "Pr&eacute;c&eacute;dent",
-                                "sNext": "Suivant",
-                                "sLast": "Dernier"
-                            },
-                            "oAria": {
-                                "sSortAscending": ": activer pour trier la colonne par ordre croissant",
-                                "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
-                            }
-                        }
-        });
 	
-	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
+	$('.data-table').dataTable({
+		"bJQueryUI": true,
+		"sPaginationType": "full_numbers",
+		"sDom": '<""l>t<"F"fp>',
+                       "iDisplayLength": 20,
+                       "aaSorting": [],
+                       "oLanguage": {
+                           "sProcessing": "Traitement en cours...",
+                           "sSearch": "Filtrer&nbsp;:",
+                           "sLengthMenu": "Afficher _MENU_ &eacute;l&eacute;ments",
+                           "sInfo": "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                           "sInfoEmpty": "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                           "sInfoFiltered": "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                           "sInfoPostFix": "",
+                           "sLoadingRecords": "Chargement en cours...",
+                           "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                           "sEmptyTable": "Aucune donnée disponible dans le tableau",
+                           "oPaginate": {
+                               "sFirst": "Premier",
+                               "sPrevious": "Pr&eacute;c&eacute;dent",
+                               "sNext": "Suivant",
+                               "sLast": "Dernier"
+                           },
+                           "oAria": {
+                               "sSortAscending": ": activer pour trier la colonne par ordre croissant",
+                               "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
+                           }
+                       }
+	});
+	
+	var checkboxClass = 'icheckbox_flat-blue';
+	var radioClass = 'iradio_flat-blue';
+	$('input[type=checkbox],input[type=radio]').iCheck({
+    	checkboxClass: checkboxClass,
+    	radioClass: radioClass
+	});
 	
 	$('select').select2();
 	
-	$("span.icon input:checkbox, th input:checkbox").click(function() {
+
+	$("span.icon input:checkbox, th input:checkbox").on('ifChecked || ifUnchecked',function() {
 		var checkedStatus = this.checked;
 		var checkbox = $(this).parents('.widget-box').find('tr td:first-child input:checkbox');		
 		checkbox.each(function() {
 			this.checked = checkedStatus;
 			if (checkedStatus == this.checked) {
-				$(this).closest('.checker > span').removeClass('checked');
+				$(this).closest('.' + checkboxClass).removeClass('checked');
 			}
 			if (this.checked) {
-				$(this).closest('.checker > span').addClass('checked');
+				$(this).closest('.' + checkboxClass).addClass('checked');
 			}
 		});
 	});	
