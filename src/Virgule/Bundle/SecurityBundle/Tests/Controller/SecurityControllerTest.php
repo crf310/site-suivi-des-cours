56a7b715cd792f1d9780c06c3a8cec560a184cb3
Guillaume Lucazeau <glucazeau@gmail.com>
2013-11-12 00:01:07 +0100

    Fix SecurityControllerTest

--- src/Virgule/Bundle/SecurityBundle/Tests/Controller/SecurityControllerTest.php
+++ src/Virgule/Bundle/SecurityBundle/Tests/Controller/SecurityControllerTest.php
@@ -13,18 +13,17 @@ class SecurityControllerTest extends WebTestCase {
         
         // Check default URL
         $crawler = $client->request('GET', '/');
-        //$this->assertEquals(302, $client->getResponse()->getStatusCode());
-        //$this->assertTrue($client->getResponse()->isRedirection('/login'));
-        
+        $this->assertTrue($crawler->filter('html:contains("Service alphabétisation")')->count() == 1);
+                
         $crawler = $client->request('GET', '/login');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
+        $this->assertTrue($crawler->filter('html:contains("Service alphabétisation")')->count() == 1);
+        
         // Test wrong credentials
         $crawler = $this->fillAndSubmitLoginForm($client, $crawler, 'Toto', 'Toto');
         
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        //$this->logResponse($client);
-        $this->assertTrue($crawler->filter('html:contains("identification a échoué")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Service alphabétisation")')->count() == 1);
         
         $crawler = $this->fillAndSubmitLoginForm($client, $crawler, 'prof1', 'password');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
