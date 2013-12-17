a52331cf171d31f997b3af7fe62fdbbe28c7c042
Guillaume Lucazeau <glucazeau@gmail.com>
2013-12-17 22:44:59 +0100

    Highlight username created

--- src/Virgule/Bundle/MainBundle/Controller/TeacherController.php
+++ src/Virgule/Bundle/MainBundle/Controller/TeacherController.php
@@ -125,7 +125,7 @@ class TeacherController extends AbstractVirguleController {
             $em->persist($entity);
             $em->flush();
             
-            $this->addFlash( 'Compte utilisateur "' . $entity->getUsername() . '" créé avec succès !');
+            $this->addFlash( 'Compte utilisateur <strong>' . $entity->getUsername() . '</strong> créé avec succès !');
             
             return $this->redirect($this->generateUrl('teacher_index'));
         }
