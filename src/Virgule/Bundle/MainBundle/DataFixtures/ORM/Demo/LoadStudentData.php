5b444e212f2b7c2f19c21c1174e5804983065536
Guillaume Lucazeau <glucazeau@gmail.com>
2014-03-01 00:27:24 +0100

    Use Faker for random students info

--- src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadStudentData.php
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Demo/LoadStudentData.php
@@ -17,19 +17,19 @@ use Virgule\Bundle\MainBundle\Entity\ClassLevelSuggested;
 class LoadStudentData extends AbstractFixture implements OrderedFixtureInterface {
 
     public function load(ObjectManager $manager) {
-
+        $fakerFr = \Faker\Factory::create('fr_FR');
+        
+        
         $countryCodes = Array('af','za','al','dz','ao','am','at','az','bd','by','bj','bt','bo','br','bg','kh','cm','cv','cl','cn','co',
                             'km','cg','cd','kr','ci','hr','do','eg','ae','ec','er','es','et','fr','ge','gt','gn','ht','hk','in',
                             'id','ir','iq','il','it','jm','jp','kz','ke','kg','lv','lb','lr','mk','ml','ma','mu','mr','md','mn','mm',
                             'np','ne','ng','uz','pk','ps','pe','ph','pl','pt','ro','ru','rw','sn','rs','sl','sk','sd','lk','se','sy',
                             'cz','th','tn','tr','ua','ve','vn','mg','cn-54');
+        
+        $locales = Array('cs_CZ', 'en_PH', 'lv_LV', 'de_DE', 'da_DK', 'ja_JP', 'uk_UA', 'de_AT', 'sr_Cyrl_RS', 'bg_BG', 'pt_BR', 'ru_RU', 'sk_SK', 'en_ZA', 'en_US', 'el_GR', 'hy_AM', 'ro_RO', 'is_IS', 'es_PE', 'nl_NL', 'sr_Latn_RS', 'it_IT', 'ro_MD', 'nl_BE', 'fr_FR', 'en_CA', 'pl_PL', 'fi_FI', 'sr_RS', 'en_GB', 'fr_BE', 'en_AU', 'es_AR', 'tr_TR', 'zh_CN', 'es_ES');
+        $nbLocales = count($locales);
+        
         $genders = Array('F', 'M');
-        $firstnames = Array('Jean', 'John', 'Juan', 'Xiao', 'Augustin', 'Dimitri', 'Sergiy', 'Ali', 'Abdel', 'Linus', 'Zinedine', 'Pol', 'Anas', 'Jean-Marc', 'Auguste', 'Zhen');
-        $lastnames = Array('Dupont', 'Smith', 'Suarez', 'Lee', 'Ranaly', 'Serpov', 'Karabatic', 'Bongo', 'Serafi', 'Zidane', 'Bellaloui', 'Lopez', 'Eriksson', 'Torvalds', 'Larsson', 'Soualem');
-
-        $nbFirstNames = count($firstnames) - 1;
-        $nbLastNames = count($lastnames) - 1;
-        $nbCountries = count($countryCodes) - 1;
         $nbCourses = 12;
 
         $classLevels = Array('A1', 'A2', 'B1/1', 'B1/2', 'B2', 'B3');
@@ -39,10 +39,29 @@ class LoadStudentData extends AbstractFixture implements OrderedFixtureInterface
 
         $nbStudents = 0;
         for ($i = 1; $i <= 234; $i++) {
+            $locale = $locales[rand(0, $nbLocales-1)];            
+            $faker = \Faker\Factory::create($locale);
+            
             $s = new Student();
-            $s->setFirstname($firstnames[rand(0, $nbFirstNames)]);
-            $s->setLastname($lastnames[rand(0, $nbLastNames)]);
-            $s->setGender($genders[rand(0, 1)]);
+            
+            $gender = $genders[rand(0, 1)];
+            $s->setGender($gender);
+            
+            if ($gender == 'F') {
+                if (isset($faker->firstNameFemale)) {
+                    $fname = $faker->firstNameFemale;
+                } else {
+                    $fname = $faker->firstName;
+                }
+            } else {
+                if (isset($faker->firstNameMale)) {
+                    $fname = $faker->firstNameMale;
+                } else {
+                    $fname = $faker->firstName;
+                }         
+            }
+            $s->setFirstname($fname);
+            $s->setLastname($faker->lastName);
 
             if (rand(1,50) < 25) {
                 $s->setPath('jpg');
@@ -54,20 +73,23 @@ class LoadStudentData extends AbstractFixture implements OrderedFixtureInterface
             $timestamp = strtotime($d . '-' . $m . '-' . $y);
             $s->setBirthdate(new \DateTime("@$timestamp"));
 
-            $rand = rand(0, $nbCountries);
-            $rc = $countryCodes[$rand];
-            $s->setNativeCountry($rc);
+            if (in_array(substr($locale, 0, 2), $countryCodes)) {
+                $s->setNativeCountry(substr($locale, 0, 2));
+            } else {
+                $s->setNativeCountry($countryCodes[rand(0, count($countryCodes)-1)]);
+            }
             
             $y = rand(2010, 2012);
             $m = rand(01, 12);
             $d = rand(01, 30);
             $timestamp = strtotime($d . '-' . $m . '-' . $y);
             $s->setRegistrationDate(new \DateTime('now'));
-            $s->setPhoneNumber("0102030405");
-            $s->setCellphoneNumber("0607080910");
-            $s->setAddress("12 rue Georges Meynieu");
-            $s->setZipcode("75010");
-            $s->setCity("Paris");
+            
+            $s->setPhoneNumber('0102030405');
+            $s->setCellphoneNumber('0605040302');
+            $s->setAddress($fakerFr->streetAddress);
+            $s->setZipcode($fakerFr->postcode);
+            $s->setCity($fakerFr->city);
 
             $s->setWelcomedByTeacher($this->getReference('prof' . rand(1, 50)));
             $s->setWelcomedInOrganizationBranch($this->getReference('deleg-3-10'));
