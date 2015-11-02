6b8fae55a29e2a82d9c7189ce0b06a0e6889ac07
Guillaume Lucazeau <glucazeau@gmail.com>
2015-11-02 22:40:08 +0100

    Ajout fixtures pour tests

---
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Test/LoadOrganizationBranchData.php
@@ -0,0 +1,36 @@
+<?php
+namespace Virgule\Bundle\MainBundle\DataFixtures\ORM\Test;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Virgule\Bundle\MainBundle\Entity\OrganizationBranch;
+
+/**
+ * Description of LoadTeacherData
+ *
+ * @author Guillaume Lucazeau
+ */
+
+class LoadOrganizationBranchData extends AbstractFixture implements OrderedFixtureInterface {
+    public function load(ObjectManager $manager) {       
+        $branch1 = new OrganizationBranch();
+        $branch1->setName("Delegation 1");
+        $branch1->setAddress("Adresse délégation 1");
+        $branch1->setEmailAddress("delegation1@example.com");
+        $branch1->setPhoneNumber("0102030405");
+        $branch1->setPresidentName("Président de la délégation 1");
+        
+        $manager->persist($branch1);
+        
+        $this->addReference('organization-1', $branch1);
+        
+        $manager->flush();
+    }
+    
+    public function getOrder() {
+        return 4;
+    }
+}
+
+?>
---
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Test/LoadTeacherData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Virgule\Bundle\MainBundle\DataFixtures\ORM\Test;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Virgule\Bundle\MainBundle\Entity\Teacher;
+
+/**
+ * Description of LoadTeacherData
+ *
+ * @author Guillaume Lucazeau
+ */
+class LoadTeacherData extends AbstractFixture implements OrderedFixtureInterface {
+    
+    public function load(ObjectManager $manager) {
+        for ($i = 1; $i <= 5; $i++) {
+          $this->createTeacher($manager, $i, true);
+        }
+        
+        for ($i = 6; $i <= 7; $i++) {
+          $this->createTeacher($manager, $i, false);
+        }
+        
+        $manager->flush();
+    }
+
+    private function createTeacher($manager, $i, $active = true) {
+        $user = new Teacher();
+        $user->setUsername("user" . $i);
+        $user->setFirstName("User");
+        if ($active) {
+          $user->setLastName("Active " . $i);
+        } else {
+          $user->setLastName("Inactive " . $i);
+        }        
+        $user->setEmail("user" . $i . "@example.com");
+        $user->setRegistrationDate(new \DateTime('now'));
+        $user->setRole($this->getReference('user-role'));
+        $user->setEnabled(true);
+        $user->setLocked(false);
+        $user->setExpired(false);
+        $user->setCredentialsExpired(false);     
+        $user->setPlainPassword('user' . $i);
+        $user->setIsActive($active);
+        $user->addOrganizationBranch($this->getReference("organization-1"));
+        $manager->persist($user);
+    }
+    public function getOrder() {
+        return 5;
+    }
+
+}
+
+?>
