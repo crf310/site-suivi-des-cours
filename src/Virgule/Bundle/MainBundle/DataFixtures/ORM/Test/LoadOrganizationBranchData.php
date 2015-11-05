87dc1d0a95b24c87076bdd45f58072780f0bea23
Guillaume Lucazeau <glucazeau@gmail.com>
2015-11-05 11:28:46 +0100

    Ajout fixtures semestres/org_branch pour tests repository semestres

--- src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Test/LoadOrganizationBranchData.php
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Test/LoadOrganizationBranchData.php
@@ -13,21 +13,39 @@ use Virgule\Bundle\MainBundle\Entity\OrganizationBranch;
  */
 
 class LoadOrganizationBranchData extends AbstractFixture implements OrderedFixtureInterface {
-    public function load(ObjectManager $manager) {       
+    public function load(ObjectManager $manager) {
         $branch1 = new OrganizationBranch();
         $branch1->setName("Delegation 1");
         $branch1->setAddress("Adresse délégation 1");
         $branch1->setEmailAddress("delegation1@example.com");
         $branch1->setPhoneNumber("0102030405");
         $branch1->setPresidentName("Président de la délégation 1");
-        
+
+        $branch2 = new OrganizationBranch();
+        $branch2->setName("Delegation 2");
+        $branch2->setAddress("Adresse délégation 2");
+        $branch2->setEmailAddress("delegation2@example.com");
+        $branch2->setPhoneNumber("0504030201");
+        $branch2->setPresidentName("Président de la délégation 2");
+
+        $branch3 = new OrganizationBranch();
+        $branch3->setName("Delegation 3");
+        $branch3->setAddress("Adresse délégation 3");
+        $branch3->setEmailAddress("delegation3@example.com");
+        $branch3->setPhoneNumber("0504030201");
+        $branch3->setPresidentName("Président de la délégation 3");
+
         $manager->persist($branch1);
-        
+        $manager->persist($branch2);
+        $manager->persist($branch3);
+
         $this->addReference('organization-1', $branch1);
-        
+        $this->addReference('organization-2', $branch2);
+        $this->addReference('organization-3', $branch3);
+
         $manager->flush();
     }
-    
+
     public function getOrder() {
         return 1;
     }
--- src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Test/LoadSemesterData.php
+++ src/Virgule/Bundle/MainBundle/DataFixtures/ORM/Test/LoadSemesterData.php
@@ -14,23 +14,57 @@ use Virgule\Bundle\MainBundle\Entity\Semester;
 
 class LoadSemesterData extends AbstractFixture implements OrderedFixtureInterface {
     public function load(ObjectManager $manager) {
-        $startDate = new \DateTime();
-        $startDate->modify('-30 day');
-        
-        
-        $endDate = new \DateTime();
-        $endDate->modify('+30 day');
-        
+        $currentStartDate = new \DateTime();
+        $currentStartDate->modify('-30 day');
+        $currentEndDate = new \DateTime();
+        $currentEndDate->modify('+30 day');
+
+        $previousStartDate = new \DateTime();
+        $previousStartDate->modify('-90 day');
+        $previousEndDate = new \DateTime();
+        $previousEndDate->modify('-60 day');
+
         // current semester
         $currentSemester = new Semester();
-        $currentSemester->setStartDate($startDate);
-        $currentSemester->setEndDate($endDate);
+        $currentSemester->setStartDate($currentStartDate);
+        $currentSemester->setEndDate($currentEndDate);
         $currentSemester->setOrganizationBranch($this->getReference('organization-1'));
 
+        // previous semester
+        $previousSemester = new Semester();
+        $previousSemester->setStartDate($previousStartDate);
+        $previousSemester->setEndDate($previousEndDate);
+        $previousSemester->setOrganizationBranch($this->getReference('organization-1'));
+
+        // current semester (different org & start date == current date)
+        $currentSemesterOrg2 = new Semester();
+        $currentSemesterOrg2->setStartDate(new \DateTime());
+        $currentSemesterOrg2->setEndDate($currentEndDate);
+        $currentSemesterOrg2->setOrganizationBranch($this->getReference('organization-2'));
+
+        // current semester (different org & end date == current date)
+        $currentSemesterOrg3 = new Semester();
+        $currentSemesterOrg3->setStartDate($currentStartDate);
+        $currentSemesterOrg3->setEndDate(new \DateTime());
+        $currentSemesterOrg3->setOrganizationBranch($this->getReference('organization-3'));
+
+        // previous semester (different org)
+        $previousSemesterOrg2 = new Semester();
+        $previousSemesterOrg2->setStartDate($previousStartDate);
+        $previousSemesterOrg2->setEndDate($previousEndDate);
+        $previousSemesterOrg2->setOrganizationBranch($this->getReference('organization-2'));
+
         $manager->persist($currentSemester);
+        $manager->persist($previousSemester);
+        $manager->persist($currentSemesterOrg2);
+        $manager->persist($currentSemesterOrg3);
+        $manager->persist($previousSemesterOrg2);
+
         $manager->flush();
+
+
     }
-    
+
     public function getOrder() {
         return 2;
     }
