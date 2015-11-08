8f0c9151260a24f7cd63e2800bab8fe46f4e43dc
Guillaume Lucazeau <glucazeau@gmail.com>
2015-11-08 15:29:46 +0100

    Ajout test myStudents

---
+++ src/Virgule/Bundle/MainBundle/Tests/Controller/StudentControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Virgule\Bundle\MainBundle\Tests\Controller;
+
+use Virgule\Bundle\MainBundle\Tests\Controller\AbstractControllerTest;
+
+class StudentControllerTest extends AbstractControllerTest {
+
+  private $FIELD_PREFIX = 'virgule_bundle_mainbundle_studenttype';
+
+  /**
+   * @test
+   */
+  public function showMyStudents_connectedUserHasNoStudents_studentsTableIsEmpty() {
+    $this->client = static::createClient();
+    $this->crawler = $this->client->request('GET', '/');
+
+    $this->login($this->ADMIN_USERNAME, $this->ADMIN_PASSWORD);
+
+    $this->goToRoute('/student/mystudents');
+    
+    $this->assertPageContainsTitle('Mes apprenants');
+    $this->assertTrue($this->crawler->filter("span.label:contains('0')")->count() == 1, 'Students table should be empty');
+    
+    $this->logout();
+  }
+
+}
