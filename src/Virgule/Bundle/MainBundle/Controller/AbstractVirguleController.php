515bfc87e12820b28004c4bfe12d3fe76520c989
Guillaume Lucazeau <glucazeau@gmail.com>
2015-11-08 14:34:55 +0100

    Utilisation méthode privée pour récupérer les repository doctrine

--- src/Virgule/Bundle/MainBundle/Controller/AbstractVirguleController.php
+++ src/Virgule/Bundle/MainBundle/Controller/AbstractVirguleController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Virgule\Bundle\MainBundle\Controller;
 
 use Pagerfanta\Pagerfanta;
@@ -12,143 +13,177 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 /**
  * Description of VirguleController
  *
  * @author guillaume
  */
 abstract class AbstractVirguleController extends Controller {
-     
-    protected function addFlash($message, $type='success') {
-         $this->get('session')->getFlashBag()->add($type, $message);
-    }
-    
-    protected function logInfo($message) {
-        $user = $this->getConnectedUser();
-        $logger = $this->get('logger');
-        $logMsg = $user->getFirstname() . ' ' . $user->getLastname() . ' - ' . $message;
-        $logger->info($logMsg);
-    }
-    
-    protected function logDebug($message) {
-        $logger = $this->get('logger');
-        $logger->debug($message);
-    }
-    protected function logError($message) {
-        $logger = $this->get('logger');
-        $logger->error($message);
-    }
-    
-    protected function paginate($entities, $page=1) {
-        $pagerfanta = new Pagerfanta(new ArrayAdapter($entities));
-        $pagerfanta->setMaxPerPage($this->container->parameters['pager_nb_results']);
 
-        try {
-            $pagerfanta->setCurrentPage($page);
-        } catch (NotValidCurrentPageException $e) {
-            throw new NotFoundHttpException();
-        }
+  protected function addFlash($message, $type = 'success') {
+    $this->get('session')->getFlashBag()->add($type, $message);
+  }
 
-        return array('entities' => $pagerfanta);        
+  protected function logInfo($message) {
+    $user = $this->getConnectedUser();
+    $logger = $this->get('logger');
+    $logMsg = $user->getFirstname() . ' ' . $user->getLastname() . ' - ' . $message;
+    $logger->info($logMsg);
+  }
+
+  protected function logDebug($message) {
+    $logger = $this->get('logger');
+    $logger->debug($message);
+  }
+
+  protected function logError($message) {
+    $logger = $this->get('logger');
+    $logger->error($message);
+  }
+
+  protected function paginate($entities, $page = 1) {
+    $pagerfanta = new Pagerfanta(new ArrayAdapter($entities));
+    $pagerfanta->setMaxPerPage($this->container->parameters['pager_nb_results']);
+
+    try {
+      $pagerfanta->setCurrentPage($page);
+    } catch (NotValidCurrentPageException $e) {
+      throw new NotFoundHttpException();
     }
-    
-    protected function getDoctrineManager() {
-        return $em = $this->getDoctrine()->getManager();
-    }
-    
-    protected function getConnectedUser() {
-        return $this->get('security.context')->getToken()->getUser();
-    }
-    
-    protected function getSelectedSemesterId()  {
-        return $this->getRequest()->getSession()->get('currentSemester')->getId();
-    }
-    
-    protected function getSelectedSemester() {
-        $currentSemesterId = $this->getSelectedSemesterId();
-        $semesterRepository = $this->getDoctrineManager()->getRepository('VirguleMainBundle:Semester');
-        $semester = $semesterRepository->find($currentSemesterId);
-        return $semester;
-    }
-    
-    protected function getSelectedOrganizationBranchId()  {
-        return $this->getRequest()->getSession()->get('organizationBranch')->getId();
-    }
-    
-    protected function getSelectedOrganizationBranch() {
-        $obRepository = $this->getDoctrineManager()->getRepository('VirguleMainBundle:OrganizationBranch');
-        $organizationBranchId = $this->getSelectedOrganizationBranchId();
-        $organizationBranch = $obRepository->find($organizationBranchId);
-        return $organizationBranch;
-    }
-    
-    protected function getListBreak($count, $break=2) {
-        return (int)($count / $break) + $count % $break;
-    }
-    
-    /* Managers */   
-    protected function getCourseManager() {
-        return $this->get('virgule.course_manager');
-    }
-    protected function getTeacherManager() {
-        return $this->get('virgule.teacher_manager');
-    }
-    protected function getSemesterManager() {
-        return $this->get('virgule.semester_manager');
-    }
-    protected function getOpenHouseManager() {
-        return $this->get('virgule.openhouse_manager');
-    }
-    protected function getStudentManager() {
-        return $this->get('virgule.student_manager');
-    }
-    protected function getDocumentManager() {
-        return $this->get('virgule.document_manager');
-    }
-    protected function getTagManager() {
-        return $this->get('virgule.tag_manager');
-    }
-    protected function getHelpManager() {
-        return $this->get('virgule.help_manager');
-    }
-    
-    /* Repositories */
-    protected function getTeacherRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Teacher');
-    }
-    protected function getStudentRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Student');
-    }
-    protected function getCourseRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Course');
-    }
-    protected function getOpenHouseRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:OpenHouse');
-    }
-    protected function getCountryRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Country');
-    }
-    protected function getClassRoomRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:ClassRoom');
-    }    
-    protected function getClassLevelRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:ClassLevel');
-    }
-    protected function getClassLevelSuggestedRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:ClassLevelSuggested');
-    }
-    protected function getClassSessionRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:ClassSession');
-    }
-    protected function getTagRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Tag');
-    }
-    protected function getLanguageRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Language');
-    }
-    protected function getDocumentRepository() {
-        return $this->getDoctrineManager()->getRepository('VirguleMainBundle:Document');
-    }
+
+    return array('entities' => $pagerfanta);
+  }
+
+  protected function getDoctrineManager() {
+    return $em = $this->getDoctrine()->getManager();
+  }
+
+  protected function getConnectedUser() {
+    return $this->get('security.context')->getToken()->getUser();
+  }
+
+  protected function getSelectedSemesterId() {
+    return $this->getRequest()->getSession()->get('currentSemester')->getId();
+  }
+
+  protected function getSelectedSemester() {
+    $currentSemesterId = $this->getSelectedSemesterId();
+    $semesterRepository = $this->getSemesterRepository();
+    $semester = $semesterRepository->find($currentSemesterId);
+    return $semester;
+  }
+
+  protected function getSelectedOrganizationBranchId() {
+    return $this->getRequest()->getSession()->get('organizationBranch')->getId();
+  }
+
+  protected function getSelectedOrganizationBranch() {
+    $obRepository = $this->getOrganizationBranchRepository();
+    $organizationBranchId = $this->getSelectedOrganizationBranchId();
+    $organizationBranch = $obRepository->find($organizationBranchId);
+    return $organizationBranch;
+  }
+
+  protected function getListBreak($count, $break = 2) {
+    return (int) ($count / $break) + $count % $break;
+  }
+
+  /* Managers */
+  protected function getCourseManager() {
+    return $this->get('virgule.course_manager');
+  }
+
+  protected function getTeacherManager() {
+    return $this->get('virgule.teacher_manager');
+  }
+
+  protected function getSemesterManager() {
+    return $this->get('virgule.semester_manager');
+  }
+
+  protected function getOpenHouseManager() {
+    return $this->get('virgule.openhouse_manager');
+  }
+
+  protected function getStudentManager() {
+    return $this->get('virgule.student_manager');
+  }
+
+  protected function getDocumentManager() {
+    return $this->get('virgule.document_manager');
+  }
+
+  protected function getTagManager() {
+    return $this->get('virgule.tag_manager');
+  }
+
+  protected function getHelpManager() {
+    return $this->get('virgule.help_manager');
+  }
+
+  /* Repositories */
+
+  private function getDoctrineRepository($entityName) {
+    return $this->getDoctrineManager()->getRepository('VirguleMainBundle:' . $entityName);
+  }
+
+  protected function getTeacherRepository() {
+    $this->getDoctrineRepository('Teacher');
+  }
+
+  protected function getStudentRepository() {
+    $this->getDoctrineRepository('Student');
+  }
+
+  protected function getCourseRepository() {
+    $this->getDoctrineRepository('Course');
+  }
+
+  protected function getOpenHouseRepository() {
+    $this->getDoctrineRepository('OpenHouse');
+  }
+
+  protected function getCountryRepository() {
+    $this->getDoctrineRepository('Country');
+  }
+
+  protected function getClassRoomRepository() {
+    $this->getDoctrineRepository('ClassRoom');
+  }
+
+  protected function getClassLevelRepository() {
+    $this->getDoctrineRepository('ClassLevel');
+  }
+
+  protected function getClassLevelSuggestedRepository() {
+    $this->getDoctrineRepository('ClassLevelSuggested');
+  }
+
+  protected function getClassSessionRepository() {
+    $this->getDoctrineRepository('ClassSession');
+  }
+
+  protected function getTagRepository() {
+    $this->getDoctrineRepository('Tag');
+  }
+
+  protected function getLanguageRepository() {
+    $this->getDoctrineRepository('Language');
+  }
+
+  protected function getDocumentRepository() {
+    $this->getDoctrineRepository('Document');
+  }
+  
+  protected function getOrganizationBranchRepository() {
+    $this->getDoctrineRepository('OrganizationBranch');
+  }
+  
+  protected function getSemesterRepository() {
+    $this->getDoctrineRepository('Semester');
+  }
+
 }
 
 ?>
