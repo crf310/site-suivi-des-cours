b78aed70d2fb844ded8a9a1f9d25d5c231da4140
Guillaume Lucazeau <glucazeau@gmail.com>
2015-11-09 22:30:18 +0100

    Ajout tests cours

--- src/Virgule/Bundle/MainBundle/Tests/Repository/CourseRepositoryTest.php
+++ src/Virgule/Bundle/MainBundle/Tests/Repository/CourseRepositoryTest.php
@@ -4,7 +4,9 @@ namespace Virgule\Bundle\MainBundle\Tests\Repository;
 use Virgule\Bundle\MainBundle\Tests\Repository\AbstractRepositoryTest;
 
 class CourseRoomRepositoryTest extends AbstractRepositoryTest {
-
+    // user avec cours
+    var $TEACHER_ID = 2;
+    
     private function getRepository() {
         return $this->_em->getRepository('VirguleMainBundle:Course');
     }
@@ -187,6 +189,55 @@ class CourseRoomRepositoryTest extends AbstractRepositoryTest {
       $result = $this->getRepository()->getNumberOfOverlappingCourses($courseId, $semesterId, $dayOfWeek, $classRoomId, $startTime, $endTime);
       $this->assertEquals(0, $result['nb_courses'], "Incorrect number of courses found");
     }
-
+    
+    /**
+     * @test
+     */
+    public function getCoursesIdsByTeacher_teacherHasTwoCoursesForThisSemester_expectedCourseIdsReturned() {
+      $semesterId = 1;
+      $expectedIds = Array(1, 2);
+      
+      $results = $this->getRepository()->getCoursesIdsByTeacher($semesterId, $this->TEACHER_ID);
+      
+      $this->assertEquals(2, count($results), "Wrong number of courses returned");
+      
+      foreach ($results as $course) {
+        $this->assertTrue(in_array($course['id'], $expectedIds), "Course #" . $course['id'] . " not expected");
+      }
+    }
+    
+    /**
+     * @test
+     */
+    public function getCoursesIdsByTeacher_teacherHasNoCourseForThisSemester_noCourseIdReturned() {
+      $semesterId = 22222;
+      $results = $this->getRepository()->getCoursesIdsByTeacher($semesterId, $this->TEACHER_ID);
+      
+      $this->assertEquals(0, count($results), "Wrong number of courses returned");
+    }
+    
+    /**
+     * @test
+     */
+    public function getCoursesByTeacher_teacherHasTwoCoursesForThisSemester_expectedCoursesReturned() {
+      $semesterId = 1;
+      $expectedIds = Array(1, 2);
+      
+      $results = $this->getRepository()->getCoursesByTeacher($semesterId, $this->TEACHER_ID);
+      $this->assertEquals(2, count($results), "Wrong number of courses returned");
+      foreach ($results as $course) {
+        $this->assertTrue(in_array($course->getId(), $expectedIds), "Course " . $course->getId() . " was not expected");
+      }
+    }
+    
+    /**
+     * @test
+     */
+    public function getCoursesByTeacher_teacherHasNoCourseForThisSemester_noCourseReturned() {
+      $semesterId = 22222;
+      
+      $results = $this->getRepository()->getCoursesByTeacher($semesterId, $this->TEACHER_ID);
+      $this->assertEmpty($results);
+    }
 }
 ?>
