db7535b6d5ebdb8cf82a9a0a107a72304d334a59
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2015-11-26 14:46:42 +0100

    Ajout test affichage formulaire compte rendu

---
+++ src/Virgule/Bundle/MainBundle/Tests/Controller/ClassSessionControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Virgule\Bundle\MainBundle\Tests\Controller;
+
+use Virgule\Bundle\MainBundle\Tests\Controller\AbstractControllerTest;
+
+class ClassSessionControllerTest extends AbstractControllerTest {
+
+  private $FIELD_PREFIX = 'virgule_bundle_mainbundle_classsessiontype';
+
+  /**
+   * @test
+   */
+  public function newAction_userCallsTheForm_newClassSessionReportFormIsDisplayed() {
+    $this->client = static::createClient();
+    $this->crawler = $this->client->request('GET', '/');
+
+    $this->login($this->ADMIN_USERNAME, $this->ADMIN_PASSWORD);
+
+    $this->goToRoute('classsession/add/course/1');
+
+    $this->assertPageContainsTitle('Ajouter un compte-rendu', 'Form has not been correctly displayed');
+
+    $this->logout();
+  }
+
+}
