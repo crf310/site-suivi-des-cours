e8f1c63ca1988507b31d0fa1b526580aafe2ae78
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2015-11-30 15:54:44 +0100

    Ajout test suppression apprenant

--- src/Virgule/Bundle/MainBundle/Tests/Controller/StudentControllerTest.php
+++ src/Virgule/Bundle/MainBundle/Tests/Controller/StudentControllerTest.php
@@ -3,6 +3,7 @@
 namespace Virgule\Bundle\MainBundle\Tests\Controller;
 
 use Virgule\Bundle\MainBundle\Tests\Controller\AbstractControllerTest;
+use Virgule\Bundle\MainBundle\Entity\Student;
 
 class StudentControllerTest extends AbstractControllerTest {
 
@@ -37,6 +38,35 @@ class StudentControllerTest extends AbstractControllerTest {
     $this->goToRoute('/student/new');
 
     $this->assertPageContainsTitle('Enregistrer un nouvel apprenant');
+
+    $this->logout();
+  }
+
+  /**
+   * @test
+   */
+  public function deleteAction_studentExists_studentIsDeleted() {
+    $newStudent = new Student();
+    $newStudent->setFirstname('Bob');
+    $newStudent->setLastname('La Chance');
+    $newStudent->setRegistrationDate(new \DateTime('now'));
+    self::$_em->persist($newStudent);
+    self::$_em->flush();
+    $newStudentId = $newStudent->getId();
+    $this->assertNotNull($newStudentId);
+
+    $this->client = static::createClient();
+    $this->crawler = $this->client->request('GET', '/');
+    $this->login($this->ADMIN_USERNAME, $this->ADMIN_PASSWORD);
+
+    $this->goToRoute('/student/' . $newStudentId . '/show');
+
+    $this->assertPageContainsTitle('Bob LA CHANCE');
+
+    $this->submitFormById('delete-student-form');
+
+    $this->assertPageContainsTitle('Tous les apprenants inscrits à un cours de cette session');
+    $this->goToRoute('/student/' . $newStudentId . '/show', 404);
 
     $this->logout();
   }
