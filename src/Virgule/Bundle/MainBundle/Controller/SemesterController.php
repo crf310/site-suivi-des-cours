f0641178d8971199bf656a0a653bc32e70e258bf
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2016-02-04 17:30:02 +0100

    Suppression actions inutilisées

--- src/Virgule/Bundle/MainBundle/Controller/SemesterController.php
+++ src/Virgule/Bundle/MainBundle/Controller/SemesterController.php
@@ -70,12 +70,9 @@ class SemesterController extends AbstractVirguleController {
     if (!$entity) {
       throw $this->createNotFoundException('Unable to find Semester entity.');
     }
-
-    $deleteForm = $this->createDeleteForm($id);
-
+    
     return array(
         'entity' => $entity,
-        'delete_form' => $deleteForm->createView(),
     );
   }
 
@@ -154,63 +151,5 @@ class SemesterController extends AbstractVirguleController {
     );
   }
 
-  /**
-   * Displays a form to edit an existing Semester entity.
-   *
-   * @Route("/{id}/edit", name="semester_edit")
-   * @Template()
-   */
-  public function editAction($id) {
-    $em = $this->getDoctrine()->getManager();
-
-    $entity = $em->getRepository('VirguleMainBundle:Semester')->find($id);
-
-    if (!$entity) {
-      throw $this->createNotFoundException('Unable to find Semester entity.');
-    }
-
-    $editForm = $this->createForm(new SemesterType(), $entity);
-    $deleteForm = $this->createDeleteForm($id);
-
-    return array(
-        'entity' => $entity,
-        'edit_form' => $editForm->createView(),
-        'delete_form' => $deleteForm->createView(),
-    );
-  }
-
-  /**
-   * Edits an existing Semester entity.
-   *
-   * @Route("/{id}/update", name="semester_update")
-   * @Method("POST")
-   * @Template("VirguleMainBundle:Semester:edit.html.twig")
-   */
-  public function updateAction(Request $request, $id) {
-    $em = $this->getDoctrine()->getManager();
-
-    $entity = $em->getRepository('VirguleMainBundle:Semester')->find($id);
-
-    if (!$entity) {
-      throw $this->createNotFoundException('Unable to find Semester entity.');
-    }
-
-    $deleteForm = $this->createDeleteForm($id);
-    $editForm = $this->createForm(new SemesterType(), $entity);
-    $editForm->bind($request);
-
-    if ($editForm->isValid()) {
-      $em->persist($entity);
-      $em->flush();
-
-      return $this->redirect($this->generateUrl('semester_edit', array('id' => $id)));
-    }
-
-    return array(
-        'entity' => $entity,
-        'edit_form' => $editForm->createView(),
-        'delete_form' => $deleteForm->createView(),
-    );
-  }
 
 }
